04059f96f79926634339bc2f12fdb888ced73e18
Cesar Franco <>
2019-11-19 23:16:11 -0500

    Se agrega nuevos proyectos actividad Node js

--- back_end_cesar_franco/public/app.js
+++ back_end_cesar_franco/public/app.js
@@ -1,3 +1,34 @@
+var Template = '<div class="card horizontal">' +
+               '<div class="card-image">' +
+               '<img src="images/home.jpg">' +
+               '</div>' +
+               '<div class="card-stacked">' +
+               '<div class="card-content">' +
+               '<div>' +
+               '<b>Direccion: $Direccion$ </b><p></p>' +
+               '</div>' +
+               '<div>' +
+               '<b>Ciudad: $Ciudad$ </b><p></p>' +
+               '</div>' +
+               '<div>' +
+               '<b>Telefono: $Telefono$ </b><p></p>' +
+               '</div>' +
+               '<div>' +
+               '<b>Código postal: $CP$ </b><p></p>' +
+               '</div>' +
+               '<div>' +
+               '<b>Precio: $Precio$ </b><p></p>' +
+               '</div>' +
+               '<div>' +
+               '<b>Tipo: $Tipo$ </b><p></p>' +
+               '</div>' +
+               '</div>' +
+               '<div class="card-action right-align">' +
+               '<a href="#">Ver más</a>' +
+               '</div>' +
+               '</div>' +
+               '</div>';
+
 //Inicializador del elemento Slider
 $("#rangoPrecio").ionRangeSlider({
   type: "double",
@@ -20,5 +51,43 @@ function setSearch() {
     $('#personalizada').toggleClass('invisible')
   })
 }
+
+function GetAll() {
+  let data = GetAllData()
+    data.forEach( (obj) => {
+      console.log(obj);
+    });
+}
+
+function GetAllData() {      
+  return new Promise(function (resolve, reject) {
+      fs.readFile(dataPath, 'utf8', function (err, readData) {
+          if (err) reject(err)
+          resolve(JSON.parse(readData))
+      });
+  });
+}
+
+function GetCity () {
+  return new Promise(function (resolve, reject) {
+      fs.readFile(dataPath, 'utf8', function (err, readData) {
+          if (err) reject(err)
+          resolve(JSON.parse(readData))
+      })
+  })
+}
+
+function GetType() {
+  return new Promise(function (resolve, reject) {
+      fs.readFile(dataPath, 'utf8', function (err, readData) {
+          if (err) reject(err)
+          resolve(JSON.parse(readData))
+      })
+  })
+}
+
+$("#buscar").on('click', function() {
+  GetAll()
+});
 
 setSearch()
